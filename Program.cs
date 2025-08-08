0fd9ee6ae22447af554c4620623d0e08b24b611e
Froggy1018 <jihe2023@gmail.com>
2025-08-08 16:59:28 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,10 @@
-﻿using System.Runtime.InteropServices;
-using System.Threading.Tasks.Dataflow;
+﻿string[] fraudulentOrderIDs = new string[3];
 
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
 
-// Your code goes here
-
-if (daysUntilExpiration == 0)
-{
-    Console.WriteLine("Your subscription has expired.");
-}
-
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage=20;
-}
-
-else if(daysUntilExpiration <=5)
-{
-    Console.WriteLine($"Your subscription expires in  {daysUntilExpiration} days.");
-    discountPercentage=10;
-}
-
-else if (daysUntilExpiration <=10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
-}
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
